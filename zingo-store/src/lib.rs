326189d6c4ec994a098748a55f1f63e02a55e6a5
fluidvanadium <fluidvanadium@zingolabs.org>
2024-02-08 16:19:34 +0000

    basic spec

--- zingo-store/src/lib.rs
+++ zingo-store/src/lib.rs
@@ -1,14 +1,26 @@
+// pub mod wallet;
+
+pub struct ZingoStore {
+    // the actual data, a subset of the blockchain. implements WalletRead, WalletWrite, and WalletCommitmentTrees
+    // wallet: ZingoWallet,
+    // includes chainType/used servers, behavioral options, historical prices and dates for block times
+    // context: ZingoContext,
+    // seeds, keys, and secrets
+    // capabilities: ZingoCapabilities,
+    //
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
