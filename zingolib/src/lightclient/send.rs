76990033fa7a6752ce03c9bb9d829eb2e9f02daa
fluidvanadium <fluidvanadium@zingolabs.org>
2024-11-04 21:05:11 +0000

    aha, the issue was that the function was returning the regular txid, as yet unaware of the issue. this is a demo, i will revert it to continue the experiment without the fix

--- zingolib/src/lightclient/send.rs
+++ zingolib/src/lightclient/send.rs
@@ -264,6 +264,7 @@ pub mod send_with_proposal {
                                         //         txid = reported_txid;
                                         //     }
                                         // };
+                                        txids.push(reported_txid);
                                     }
                                     Err(e) => {
                                         println!("server returned invalid txid {}", e);
@@ -272,8 +273,6 @@ pub mod send_with_proposal {
                                 }
 
                                 spend_status = Some((txid, new_status));
-
-                                txids.push(txid);
                             }
                             Err(server_err) => {
                                 return Err(BroadcastCachedTransactionsError::Broadcast(server_err))
