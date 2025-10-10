04d8ce796e2ba68f45efacec84c0abb6e9bfb0d6
fluidvanadium <fluidvanadium@zingolabs.org>
2025-10-10 05:23:12 +0000

    cargo clippy --fix --tests -- -Wclippy::pedantic

--- darkside-tests/tests/advanced_reorg_tests.rs
+++ darkside-tests/tests/advanced_reorg_tests.rs
@@ -89,9 +89,9 @@ async fn reorg_changes_incoming_tx_height() {
     let reorg_sync_result = light_client.sync_and_await().await;
 
     match reorg_sync_result {
-        Ok(value) => println!("{}", value),
-        Err(err_str) => println!("{}", err_str),
-    };
+        Ok(value) => println!("{value}"),
+        Err(err_str) => println!("{err_str}"),
+    }
 
     // Assert that balance holds
     assert_eq!(
@@ -141,7 +141,7 @@ async fn prepare_before_tx_height_change_reorg(uri: http::Uri) -> Result<(), Str
         REORG_CHANGES_INCOMING_TX_HEIGHT_BEFORE
     );
 
-    println!("dataset path: {}", dataset_path);
+    println!("dataset path: {dataset_path}");
 
     connector
         .stage_blocks_stream(read_dataset(dataset_path))
@@ -253,9 +253,9 @@ async fn reorg_changes_incoming_tx_index() {
     let reorg_sync_result = light_client.sync_and_await().await;
 
     match reorg_sync_result {
-        Ok(value) => println!("{}", value),
-        Err(err_str) => println!("{}", err_str),
-    };
+        Ok(value) => println!("{value}"),
+        Err(err_str) => println!("{err_str}"),
+    }
 
     // Assert that balance holds
     assert_eq!(
@@ -305,7 +305,7 @@ async fn prepare_before_tx_index_change_reorg(uri: http::Uri) -> Result<(), Stri
         REORG_CHANGES_INCOMING_TX_INDEX_BEFORE
     );
 
-    println!("dataset path: {}", dataset_path);
+    println!("dataset path: {dataset_path}");
 
     connector
         .stage_blocks_stream(read_dataset(dataset_path))
@@ -417,9 +417,9 @@ async fn reorg_expires_incoming_tx() {
     let reorg_sync_result = light_client.sync_and_await().await;
 
     match reorg_sync_result {
-        Ok(value) => println!("{}", value),
-        Err(err_str) => println!("{}", err_str),
-    };
+        Ok(value) => println!("{value}"),
+        Err(err_str) => println!("{err_str}"),
+    }
 
     // Assert that balance holds
     assert_eq!(
@@ -465,7 +465,7 @@ async fn prepare_expires_incoming_tx_before_reorg(uri: http::Uri) -> Result<(), 
         REORG_EXPIRES_INCOMING_TX_HEIGHT_BEFORE
     );
 
-    println!("dataset path: {}", dataset_path);
+    println!("dataset path: {dataset_path}");
 
     connector
         .stage_blocks_stream(read_dataset(dataset_path))
@@ -609,7 +609,7 @@ async fn reorg_changes_outgoing_tx_height() {
     .await
     .unwrap();
 
-    println!("SENT TX ID: {:?}", sent_tx_id);
+    println!("SENT TX ID: {sent_tx_id:?}");
 
     let mut incoming_transaction_stream = connector.get_incoming_transactions().await.unwrap();
     let tx = incoming_transaction_stream
@@ -692,9 +692,9 @@ async fn reorg_changes_outgoing_tx_height() {
     let reorg_sync_result = light_client.sync_and_await().await;
 
     match reorg_sync_result {
-        Ok(value) => println!("{}", value),
-        Err(err_str) => println!("{}", err_str),
-    };
+        Ok(value) => println!("{value}"),
+        Err(err_str) => println!("{err_str}"),
+    }
 
     let expected_after_reorg_balance = AccountBalance {
         total_sapling_balance: Some(0.try_into().unwrap()),
@@ -767,7 +767,7 @@ async fn prepare_changes_outgoing_tx_height_before_reorg(uri: http::Uri) -> Resu
         REORG_EXPIRES_INCOMING_TX_HEIGHT_BEFORE
     );
 
-    println!("dataset path: {}", dataset_path);
+    println!("dataset path: {dataset_path}");
 
     connector
         .stage_blocks_stream(read_dataset(dataset_path))
@@ -793,7 +793,7 @@ async fn prepare_changes_outgoing_tx_height_before_reorg(uri: http::Uri) -> Resu
 
 #[ignore = "darkside block continuity error, after re-org block 206's prev hash does not match 205's hash"]
 #[tokio::test]
-/// ### ReOrg Removes Outbound TxAnd Is Never Mined
+/// ### `ReOrg` Removes Outbound `TxAnd` Is Never Mined
 /// Transaction was included in a block, and then is not included in a block after a reorg, and expires.
 /// Steps:
 /// 1. create fake chain
@@ -872,7 +872,7 @@ async fn reorg_expires_outgoing_tx_height() {
     .await
     .unwrap();
 
-    println!("SENT TX ID: {:?}", sent_tx_id);
+    println!("SENT TX ID: {sent_tx_id:?}");
 
     let sent_tx_height: i32 = 205;
     _ = connector.apply_staged(sent_tx_height).await;
@@ -941,9 +941,9 @@ async fn reorg_expires_outgoing_tx_height() {
     let reorg_sync_result = light_client.sync_and_await().await;
 
     match reorg_sync_result {
-        Ok(value) => println!("{}", value),
-        Err(err_str) => println!("{}", err_str),
-    };
+        Ok(value) => println!("{value}"),
+        Err(err_str) => println!("{err_str}"),
+    }
 
     // Assert that balance is equal to the initial balance since the
     // sent transaction was never mined and has expired.
@@ -996,7 +996,7 @@ async fn reorg_expires_outgoing_tx_height() {
 ///   - Wallet has spendable funds
 ///
 /// 1. Setup w/ default dataset
-/// 2. applyStaged(received_Tx_height)
+/// 2. `applyStaged(received_Tx_height)`
 /// 3. sync up to received_Tx_height
 /// 4. create transaction
 /// 5. stage 10 empty blocks
@@ -1007,7 +1007,7 @@ async fn reorg_expires_outgoing_tx_height() {
 /// 7. sync to  sentTxHeight + 2
 /// 8. stage sentTx and otherTx at sentTxheight
 /// 9. applyStaged(sentTx + 2)
-/// 10. sync up to received_Tx_height + 2
+/// 10. sync up to `received_Tx_height` + 2
 /// 11. verify that the sent tx is mined and balance is correct
 /// 12. applyStaged(sentTx + 10)
 /// 13. verify that there's no more pending transaction
@@ -1076,7 +1076,7 @@ async fn reorg_changes_outgoing_tx_index() {
     .await
     .unwrap();
 
-    println!("SENT TX ID: {:?}", sent_tx_id);
+    println!("SENT TX ID: {sent_tx_id:?}");
 
     let mut incoming_transaction_stream = connector.get_incoming_transactions().await.unwrap();
     let tx = incoming_transaction_stream
@@ -1194,7 +1194,7 @@ async fn reorg_changes_outgoing_tx_index() {
     let after_reorg_transactions = light_client.value_transfers(true).await.unwrap();
 
     println!("post re-org value transfers:");
-    println!("{}", after_reorg_transactions);
+    println!("{after_reorg_transactions}");
     println!("post re-org tx summaries:");
     println!(
         "{}",
@@ -1236,7 +1236,7 @@ async fn test_read_block_dataset() {
         REORG_CHANGES_INCOMING_TX_HEIGHT_BEFORE
     );
     let blocks = read_dataset(dataset_path);
-    assert_eq!(blocks.len(), 21)
+    assert_eq!(blocks.len(), 21);
 }
 
 #[tokio::test]
@@ -1248,7 +1248,7 @@ async fn test_read_tree_state_from_file() {
         203
     );
 
-    println!("{}", tree_state_path);
+    println!("{tree_state_path}");
 
     let tree_state = TreeState::from_file(tree_state_path).unwrap();
 
